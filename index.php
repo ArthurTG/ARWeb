1854a487a8ea6ba7d4d1465ada9d83287976d24f
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2019-10-14 00:50:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html>
-<!-- AR.js by @jerome_etienne - github: https://github.com/jeromeetienne/ar.js - info: https://medium.com/arjs/augmented-reality-in-10-lines-of-html-4e193ea9fdbf -->
-<script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
-<script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js"></script>
+<script src="https://aframe.io/releases/0.7.0/aframe.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="aframe-ar.js"></script>
+
 <body style='margin : 0px; overflow: hidden;'>
-	<a-scene embedded arjs='sourceType: webcam;'>
-		<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-		<a-marker-camera preset='hiro'></a-marker-camera>
-	</a-scene>
-</body>
+    <a-scene stats embedded arjs='sourceType: webcam;'>
+
+        <a-assets>
+            <a-asset-item id="model" src="models/manekiinu.gltf" />
+        </a-assets>
+        <a-gltf-model src="#model" scale="0.01 0.01 0.01"></a-gltf-model>
+        <!--<a-box material='color: black;'></a-box>-->
+        <a-marker-camera preset='custom' type='pattern' patterUrl='ar.patt' url='ar.patt'>
+
+        </a-marker-camera>
+    </a-scene>
+</body
 </html>
