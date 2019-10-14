8f5a40559117830ed4b8003ecc5967ebc4d87177
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2019-10-14 12:38:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!doctype HTML>
 <html>
-<script src="https://aframe.io/releases/0.9.2/aframe.min.js"></script>
-<script src="https://raw.githack.com/jeromeetienne/AR.js/2.0.0/aframe/build/aframe-ar.js"></script>
+<script src="/A-Frame/aframe.min.js"></script>
+<script src="/A-Frame/aframe-ar.js"></script>
+<script src="/A-Frame/aframe-gif-shader.min.js"></script>
+<script src="/A-Frame/aframe-gif-component.min.js"></script>
   <body style='margin : 0px; overflow: hidden;'>
     <a-scene embedded arjs vr-mode-ui="enabled: false">
             <a-assets>
-                    <a-asset-item id="object-obj" src="BMW.obj" shadow="receive:false"></a-asset-item>
-                    <a-asset-item id="object-mtl" src="BMW.mtl"></a-asset-item>
+                    
             </a-assets>         
         <a-marker-camera preset='hiro'>
-            <a-entity id="loadingEl" obj-model="obj: #object-obj; mtl: #object-mtl" scale=".01 .01 .01" rotation="0 0 0" loader></a-entity>
+            <a-entity gltf-model="url(integra.gltf)" scale=".05 .05 .05"></a-entity>
         </a-marker-camera>
       <a-entity camera>
             
