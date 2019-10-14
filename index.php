b8b83ba59c53e047cc06ab20c9a7dd379aa6975f
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2019-10-14 13:30:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,7 @@
                     
             </a-assets>         
         <a-marker-camera preset='hiro'>
-            <!-- <a-entity gltf-model="url(integra.gltf)" scale=".05 .05 .05"></a-entity> -->
-          <a-entity geometry="primitive:plane;width:2;height:2;" position="0 0 0" rotation="270 0 0" material="shader:gif;src:url(platano.gif);alphaTest:0.5;"></a-entity>
+            <a-entity gltf-model="url(integra.gltf)" scale=".05 .05 .05"></a-entity>
         </a-marker-camera>
       <a-entity camera>
             
