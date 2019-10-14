76c73556a78035fd800c68fdd2b8adcb045500a2
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2019-10-14 13:27:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,6 @@
       <script src="/A-Frame/aframe-ar.js"></script>
       <script src="/A-Frame/aframe-gif-shader.min.js"></script>
       <script src="/A-Frame/aframe-gif-component.min.js"></script>
-      <script type="text/javascript">
-      function Redirect()
-      {
-        window.location = "https://apparweb.herokuapp.com";
-      }
-          
-      </script>
   </head>
 
   <body style='margin : 0px; overflow: hidden;'>
@@ -20,7 +13,8 @@
                     
             </a-assets>         
         <a-marker-camera preset='hiro'>
-            <a-entity gltf-model="url(integra.gltf)" scale=".05 .05 .05"></a-entity>
+            <!-- <a-entity gltf-model="url(integra.gltf)" scale=".05 .05 .05"></a-entity> -->
+          <a-entity geometry="primitive:plane;width:2;height:2;" position="0 0 0" rotation="270 0 0" material="shader:gif;src:url(platano.gif);alphaTest:0.5;"></a-entity>
         </a-marker-camera>
       <a-entity camera>
             
