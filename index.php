427a13aebe343444c2a8b0c72cd9dcf591dd9759
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2019-10-14 14:29:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,14 @@
       <script src="/A-Frame/aframe-gif-shader.min.js"></script>
       <script src="/A-Frame/aframe-gif-component.min.js"></script>
   </head>
-
   <body style='margin : 0px; overflow: hidden;'>
     <a-scene embedded arjs vr-mode-ui="enabled: false">
             <a-assets>
                     
             </a-assets>         
-        <a-marker-camera preset='hiro'>
+        <a-marker preset='hiro'>
             <a-entity gltf-model="url(integra.gltf)" scale=".05 .05 .05"></a-entity>
-        </a-marker-camera>
+        </a-marker>
       <a-entity camera>
             
       </a-entity>
