53a510c6768cbad893a0d71d4e001df3784f27d9
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2019-10-17 05:04:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <!doctype HTML>
 <html>
-<script src="https://aframe.io/releases/0.9.2/aframe.min.js"></script>
-<script src="https://raw.githack.com/jeromeetienne/AR.js/2.0.0/aframe/build/aframe-ar.js"></script>
-<body style='margin : 0px; overflow: hidden;'>
-
-  
+  <head>
+      <script src="/A-Frame/aframe.min.js"></script>
+      <script src="/A-Frame/aframe-ar.js"></script>
+      <script src="/A-Frame/aframe-gif-shader.min.js"></script>
+      <script src="/A-Frame/aframe-gif-component.min.js"></script>
+  </head>
+  <body style='margin : 0px; overflow: hidden;'>
     <a-scene embedded arjs vr-mode-ui="enabled: false">
-        <a-assets>
-            <a-asset-item id="integra" src="https://github.com/ArthurTG/ARWeb/blob/master/integra.gltf"></a-asset-item>
-        </a-assets>
-
+            <a-assets>
+                    
+            </a-assets>         
         <a-marker preset='hiro'>
-            <a-gltf-model  src="#integra" scale=".025 .025 .025"></a-gltf-model>
+            <a-entity gltf-model="url(integra.gltf)" scale=".025 .025 .025"></a-entity>
         </a-marker>
-    <a-entity camera>
+      <a-entity camera>
             
       </a-entity>
     </a-scene>
