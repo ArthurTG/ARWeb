e02d40de03ee0653388c3a2bae11e999605ebcc4
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2021-12-17 22:54:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,21 +7,14 @@
       <script src="/A-Frame/aframe-gif-component.min.js"></script>
   </head>
   <body style='margin : 0px; overflow: hidden;'>
+    
     <a-scene embedded arjs= "debugUIEnabled:false" vr-mode-ui="enabled: false">
             <a-assets>
                     
             </a-assets>   
       
-        <a-marker type='pattern' url='pattern-mente.patt'>
+        <a-marker type='pattern' url='pattern-qr-code.patt'>
             <a-gltf-model  src="integra.gltf" scale=".025 .025 .025"></a-gltf-model>
-        </a-marker>
-
-        <a-marker type='pattern' url='pattern-cuerpo.patt'>
-          <a-gltf-model  src="Mision.gltf" scale=".2 .2 .2"></a-gltf-model>
-        </a-marker>
-
-        <a-marker type='pattern' url='pattern-emociones.patt'>
-          <a-gltf-model  src="Vision.gltf" scale=".2 .2 .2"></a-gltf-model>
         </a-marker>
       
       <a-entity camera>
