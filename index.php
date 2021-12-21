b7f757710479f19585f774e48db67e2c9a98ffa9
ArthurTG <45639275+ArthurTG@users.noreply.github.com>
2021-12-21 03:26:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
                 <a-asset-item id="amelia" src="amelia_watson/scene.gltf"></a-asset-item>
             </a-assets>   
       
-        <<a-marker type='pattern' url='pattern-qr-code.patt'>
+        <<a-marker type='pattern' url='pattern-marker.patt'>
             <a-gltf-model  src="#amelia" scale=".4 .4 .4" rotation="-90. .0 .0"></a-gltf-model>
         </a-marker>
       
